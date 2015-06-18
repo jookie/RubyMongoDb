399ea58a639ba897f0d839ab7a0b45a6b20a5a88
dovy <credential.helper>
2015-06-17 21:51:17 -0400

    RubyMongoDb_2

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,70 @@
+class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @products = Product.all
+    render :json => @products, :status => :ok
+  end
+
+  def show
+    begin
+       name = retreive_product_name_by_dimention_and_weight(params[:length],
+                                                            params[:width],
+                                                            params[:heigh],
+                                                            params[:weight])
+    rescue
+      respond_info('error', 'internal_server_error', 'Show product name Failed', :internal_server_error)
+      return
+    end
+    render :json => name, :status => :ok
+  end
+
+  def create
+    @product = Product.new(product_params)
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.json { render :show, status: :created, location: @product }
+      else
+        format.html { render :new }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    begin
+      @product = Product.find(params[:id])
+    rescue
+      respond_info('error', 'internal_server_error', 'Update product Failed', :internal_server_error)
+      return
+    end
+    if @product.update(product_params)
+      render :json => @products, :status => :no_content #HTTP status code: 204 No Content
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    begin
+      @product = Product.find(params[:id])
+    rescue
+      respond_info('error', 'internal_server_error', 'Delete product Failed', :internal_server_error)
+      return
+    end
+    @product.destroy
+    respond_info('success', 'success', 'Success', :no_content) #HTTP status code: 204 No Content
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def product_params
+      params.require(:product).permit(:name, :length, :width, :height, :weight)
+    end
+end
---
+++ app/models/product.rb
@@ -0,0 +1,48 @@
+#models/product.rb
+name:String
+field :length, type: Float
+field :width,  type: Float
+field :height, type: Float
+field :weight, type: Float
+class Product
+  attr_accessible :name, :length, :width, :height, :weight
+
+  include Mongoid::Document
+  field :name,   type: String
+  field :length, type: Float
+  field :width,  type: Float
+  field :height, type: Float
+  field :weight, type: Float
+
+  field :delta,  type: Float, :allow_nil => true
+
+  # All dimensions must be present in order for shipping information to be effective.
+  validate :length, presence: true, numericality: { greater_than: 0.01,
+                                                                  :message   => "Invalid length."}
+  validate :width , presence: true, numericality: { greater_than: 0.01,
+                                                                  :greater_than_or_equal_to => :length,
+                                                                  :message   => "Invalid width."}
+  validate :height, presence: true, numericality: { greater_than: 0.01,
+                                                                  :message   => "Invalid height."}
+
+  # Shows ONE product that best matches a given length/width/height/weight query
+  # For example:
+  # if I make an API request for a product with the following dimensions: 48”l X 14”w X 12”h (@ 42lbs)
+  # the API should send me back “Golf - Small”
+  def retreive_product_name_by_dimention_and_weight(parm_length, parm_width, parm_height, parm_weight)
+    #These are the other options to try implement the search
+    #Product.where([] => [parm_length, parm_width, parm_height])
+    #Product.where(:length.gte => parm_length).and(:width.gte => parm_width).
+    # and(:height.gte => parm_height).and(:parm_weight.gte => parm_weight)
+    products = Product.all_of(:length.gte=> parm_length,
+                              :width.gte => parm_width,
+                              :height.gte=> parm_height,
+                              :weight    => parm_weight)
+    products.each do |product|
+        product.delta = product.length+product.width+product.height+product.weight-
+                        parm_length-parm_width-parm_height-parm_weight
+    end
+    Product.descending(:delta)
+    return Product.first.name
+  end
+end
